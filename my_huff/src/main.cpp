5b458a448994ea221f4e779b3582277879bd0c34
光超 李 <812624252@qq.com>
2022-06-18 19:31:03 +0800

    修改

--- my_huff/src/main.cpp
+++ my_huff/src/main.cpp
@@ -5,9 +5,9 @@ int main()
 {
 	Encoder en;
 	Decoder de;
-	string source =R"(.\img\2.bmp)"; //目标文件
-	string temp = R"(.\output\2.bin)"; //压缩后文件
-	string target = R"(.\output\2.bmp)"; //解压后文件
+	string source =R"(.\img\home.bmp)"; //目标文件
+	string temp = R"(.\output\home.bin)"; //压缩后文件
+	string target = R"(.\output\home.bmp)"; //解压后文件
 	en.compress(source, temp);
 	de.deCompress(temp, target);
 	return 1;
